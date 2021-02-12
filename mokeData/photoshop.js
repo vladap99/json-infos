a116eb4b0d62b86397182646b3fd2696ffdaf618
gabiap96 <apostol.gabriel90@gmail.com>
2021-02-12 11:58:28 +0200

    uff

---
+++ mokeData/photoshop.js
@@ -0,0 +1,57 @@
+export default {
+  data: [
+    {
+      id: "art-1",
+      label: "Default",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+      link: "https://images4.alphacoders.com/576/thumb-1920-57696.jpg"
+    },
+    {
+      id: "art-2",
+      label: "Anime",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+      link:
+        "https://res.cloudinary.com/djdx2x303/image/upload/v1612874981/Test-img/1551490323908_mt7bth.png"
+    },
+    {
+      id: "art-1",
+      label: "Default",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+      link: "https://images4.alphacoders.com/576/thumb-1920-57696.jpg"
+    },
+    {
+      id: "art-2",
+      label: "Anime",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+      link:
+        "https://res.cloudinary.com/djdx2x303/image/upload/v1612874981/Test-img/1551490323908_mt7bth.png"
+    },
+    {
+      id: "art-2",
+      label: "Anime",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+      link:
+        "https://res.cloudinary.com/djdx2x303/image/upload/v1612874981/Test-img/1551490323908_mt7bth.png"
+    },
+    {
+      id: "art-3",
+      label: "Anime-2",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+      link:
+        "https://res.cloudinary.com/djdx2x303/image/upload/v1612874967/Test-img/177546_hzjyqo.jpg"
+    },
+    {
+      id: "art-1",
+      label: "Default",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+      link: "https://images4.alphacoders.com/576/thumb-1920-57696.jpg"
+    },
+    {
+      id: "art-3",
+      label: "Anime-2",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+      link:
+        "https://res.cloudinary.com/djdx2x303/image/upload/v1612874967/Test-img/177546_hzjyqo.jpg"
+    }
+  ]
+};
